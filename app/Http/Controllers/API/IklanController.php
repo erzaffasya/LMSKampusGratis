aabcbc9c6822d6fc130891a40b6a9ba8b0fce671
erzaffasya <58721335+erzaffasya@users.noreply.github.com>
2021-10-22 12:35:49 +0700

    Revert "Merge branch 'staging' of https://gitlab.com/k970/web-admin-be into staging"
    This reverts commit 668360bb48ebb9aea6d562ac8d3a1f7afd655cbc, reversing
    changes made to c39c33a97de88151d36ec28c2fb609aa15f966eb.

--- app/Http/Controllers/API/IklanController.php
+++ app/Http/Controllers/API/IklanController.php
@@ -102,7 +102,7 @@ class IklanController extends Controller
     {
         $iklan = Iklan::find($id);
         $lst = explode('/', $iklan->gambar);
-        $txt = 'api/view2/'.$lst[0];
+        $txt = 'api/download/'.$lst[0];
         return redirect($txt);
     }
 
@@ -110,9 +110,7 @@ class IklanController extends Controller
     {
         $iklan = Iklan::find($id);
         $lst = explode('/', $iklan->gambar);
-        //$txt = 'api/view2/'.$lst[0];
-        $filename = $lst[0];
-        $txt = '/storage/documents/'. $filename;
+        $txt = 'api/view2/'.$lst[0];
         return redirect($txt);
     }
 }
