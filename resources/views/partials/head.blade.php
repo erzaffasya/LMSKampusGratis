fb0377864a6d6a35a2368b793220d6356eb3f14c
erzaffasya <58721335+erzaffasya@users.noreply.github.com>
2021-10-26 13:04:47 +0700

    up

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -15,7 +15,7 @@
   <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
   <link href="{{asset('assets/css/nucleo-svg.css')}}" rel="stylesheet" />
   <!-- CSS Files -->
-  <link id="pagestyle" href="{{('assets/css/soft-ui-dashboard.css?v=1.0.4')}}" rel="stylesheet" />
+  <link id="pagestyle" href="{{asset('assets/css/soft-ui-dashboard.css?v=1.0.4')}}" rel="stylesheet" />
 
     <link href="{{asset('css/app.css')}}" rel="stylesheet" />
 
@@ -23,4 +23,3 @@
     @stack('css')
 </head>
 
-  
