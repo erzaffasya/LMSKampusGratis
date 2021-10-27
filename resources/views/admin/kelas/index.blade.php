d173b04b928430a098c763b27357f41b5553465d
erzaffasya <58721335+erzaffasya@users.noreply.github.com>
2021-10-27 09:47:35 +0700

    push

--- resources/views/admin/kelas/index.blade.php
+++ resources/views/admin/kelas/index.blade.php
@@ -11,65 +11,63 @@
           </div>
         </div>
       </div>
-      <div class="card-body p-3">
-        <div class="row">
-          <div class="table-responsive p-0">
-            <table id="datatable-search" class="table align-items-center mb-0">
+      <div class="card-body  px-0 pt-0 pb-2">
 
-              <thead>
-                <tr>
-                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">No</th>
-                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nama</th>
-                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Deskripsi</th>
-                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Aksi</th>
-                </tr>
-              </thead>
+        <table id="datatable-search" class="table align-items-center mb-0">
 
-              <tbody>
-                @foreach ($kelas as $item)
-                <tr>
-                    <td class="align-middle text-center">
-                      <span class="text-secondary text-xs font-weight-bold">{{ $loop->iteration }}</span>
-                    </td>
-                  <td class="align-middle text-center">
-                    <p class="text-xs font-weight-bold mb-0">{{ $item->nama }}</p>
-                  </td>
-                  <td class="align-middle text-center">
-                    <span class="text-secondary text-xs font-weight-bold">{!! $item->deskripsi !!}</span>
-                  </td>
-                  <td>
-                    <div class="ms-auto text-center">
-                      <form action="{{route('kelas.destroy', $item->id)}}" method="POST" style="display: inline">
-                        @csrf
-                        @method("DELETE")
-                        <button type="submit" class="btn btn-link text-danger text-gradient px-3 mb-0"><i class="fas fa-trash text-secondary"></i></button>
-                      </form>
-                      <a class="btn btn-link text-dark px-3 mb-0" href="{{route('kelas.edit', $item->id)}}"><i class="fas fa-user-edit text-secondary"></i></a>
-                      <a class="btn btn-link text-dark px-3 mb-0" href="{{route('kelas.show', $item->id)}}"><i class="fas fa-eye text-secondary"></i></a>
-                    </div>
-                  </td>
-                </tr>
-                {{-- <tr>
+          <thead>
+            <tr>
+              <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">No</th>
+              <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nama</th>
+              <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Deskripsi</th>
+              <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Aksi</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            @foreach ($kelas as $item)
+            <tr>
+              <td class="align-middle text-center">
+                <span class="text-secondary text-xs font-weight-bold">{{ $loop->iteration }}</span>
+              </td>
+              <td class="align-middle text-center">
+                <p class="text-xs font-weight-bold mb-0">{{ $item->nama }}</p>
+              </td>
+              <td class="align-middle text-center">
+                <span class="text-secondary text-xs font-weight-bold">{!! $item->deskripsi !!}</span>
+              </td>
+              <td>
+                <div class="ms-auto text-center">
+                  <form action="{{route('kelas.destroy', $item->id)}}" method="POST" style="display: inline">
+                    @csrf
+                    @method("DELETE")
+                    <button type="submit" class="btn btn-link text-danger text-gradient px-3 mb-0"><i class="fas fa-trash text-secondary"></i></button>
+                  </form>
+                  <a class="btn btn-link text-dark px-3 mb-0" href="{{route('kelas.edit', $item->id)}}"><i class="fas fa-user-edit text-secondary"></i></a>
+                  <a class="btn btn-link text-dark px-3 mb-0" href="{{route('kelas.show', $item->id)}}"><i class="fas fa-eye text-secondary"></i></a>
+                </div>
+              </td>
+            </tr>
+            {{-- <tr>
                   <td>{{ $loop->iteration }}</td>
-                  <td>{{ $item->nama }}</td>
-                  <td>{!! $item->deskripsi !!}</td>
-                  <td>
-                    <div class="btn-group" role="group">
-                      <a href="{{route('kelas.edit', $item->id)}}" class="btn btn-primary"><i class="material-icons">edit</i></a>
-                      <form action="{{route('kelas.destroy', $item->id)}}" method="POST">
-                        @csrf
-                        @method("DELETE")
-                        <button type="submit" class="btn btn-danger"><i class="material-icons">delete</i></button>
-                      </form>
-                    </div>
-                  </td>
-                </tr> --}}
-                @endforeach
-              </tbody>
+            <td>{{ $item->nama }}</td>
+            <td>{!! $item->deskripsi !!}</td>
+            <td>
+              <div class="btn-group" role="group">
+                <a href="{{route('kelas.edit', $item->id)}}" class="btn btn-primary"><i class="material-icons">edit</i></a>
+                <form action="{{route('kelas.destroy', $item->id)}}" method="POST">
+                  @csrf
+                  @method("DELETE")
+                  <button type="submit" class="btn btn-danger"><i class="material-icons">delete</i></button>
+                </form>
+              </div>
+            </td>
+            </tr> --}}
+            @endforeach
+          </tbody>
 
-            </table>
-          </div>
-        </div>
+        </table>
+
       </div>
     </div>
   </div>
